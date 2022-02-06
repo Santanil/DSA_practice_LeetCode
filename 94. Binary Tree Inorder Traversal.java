06b9b9aa6a3d2b5e750eaa9cc19e91e155ec69e7
SANTANIL BASAK <santanilbasak@gmail.com>
2022-02-06 18:57:00 +0530

    Update 94. Binary Tree Inorder Traversal.java

--- 94. Binary Tree Inorder Traversal.java
+++ 94. Binary Tree Inorder Traversal.java
@@ -1,8 +1,3 @@
-//94. Binary Tree Inorder Traversal
-//https://leetcode.com/problems/binary-tree-inorder-traversal/
-//https://docs.google.com/spreadsheets/d/1Hk2dxaDNsqrDElQy5NKMWKAdYzjgWrmoFgT-vWRHkC8/edit#gid=0
-
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -32,5 +27,36 @@ class Solution {
         List<Integer> res=new ArrayList<>()    ;
         res=inorder(root,res);
         return res;
+    }
+}
+
+
+//Morris Traversal
+//TC:O(N)   SC:O(1)
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorder=new ArrayList<>()    ;
+        TreeNode cur=root;
+        while(cur!=null){
+            if(cur.left==null){  //don't have left node so add the current root node and move right
+                inorder.add(cur.val);
+                cur=cur.right;
+            }else{
+                TreeNode prev=cur.left;
+                while(prev.right!=null && prev.right!=cur){ //move to the right most node of the left subtree of the cur node
+                    prev=prev.right;
+                }
+                
+                if(prev.right==null){ //if the right of the rightmost node ==null then create a thread to node
+                    prev.right=cur;
+                    cur=cur.left;
+                }else{  // thread exist so destroy it and add the cur root value and then move right
+                    prev.right=null;
+                    inorder.add(cur.val);
+                    cur=cur.right;
+                }
+            }
+        }
+        return inorder;
     }
 }
