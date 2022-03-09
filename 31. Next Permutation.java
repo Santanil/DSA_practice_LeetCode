74fb29811fb934fb4fb5d3e677de594265cfcde9
SANTANIL BASAK <santanilbasak@gmail.com>
2022-03-09 23:07:53 +0530

    Create 31. Next Permutation.java

---
+++ 31. Next Permutation.java
@@ -0,0 +1,37 @@
+//31. Next Permutation
+//https://leetcode.com/problems/next-permutation/
+//https://www.youtube.com/watch?v=LuLCLgMElus
+//
+
+
+BF Approach: To generate all the permuations with the given input and arrange them in lexecogrphical order and then find the next element
+TC:O(N!*N)
+  
+Optimized Approach
+//TC:O(N)
+//SC:O(1)
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums.length==0 || nums.length==1)
+            return;
+        int i=nums.length-2,n=nums.length;
+        while(i>=0 && nums[i]>=nums[i+1])
+            i--;
+        if(i>=0){
+            int x=n-1;
+            while(x>=0 && nums[x]<=nums[i])
+                x--;
+            swap(i,x,nums);
+        }
+        reverse(i+1,n-1,nums);
+    }
+    public void swap(int i,int j,int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void reverse(int i, int j,int[] nums){
+        while(i<j)
+            swap(i++,j--,nums);     
+    }
+}
