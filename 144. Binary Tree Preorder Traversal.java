070e3f3115adc0104c8f4502570d1905a4dd9c8d
SANTANIL BASAK <santanilbasak@gmail.com>
2022-03-18 12:46:14 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- 144. Binary Tree Preorder Traversal.java
+++ 144. Binary Tree Preorder Traversal.java
@@ -1,6 +1,7 @@
 //144. Binary Tree Preorder Traversal
 //https://leetcode.com/problems/binary-tree-preorder-traversal/
-//https://www.youtube.com/watch?v=RlUu72JrOCQ&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=6
+recursive->//https://www.youtube.com/watch?v=RlUu72JrOCQ&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=6
+iterative->//https://www.youtube.com/watch?v=Bfqd8BsPVuw&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=10
 //https://docs.google.com/spreadsheets/d/1Hk2dxaDNsqrDElQy5NKMWKAdYzjgWrmoFgT-vWRHkC8/edit#gid=0
 
 
@@ -34,5 +35,26 @@ class Solution {
         list.add(root.val);
         preorder(root.left,list);
         preorder(root.right,list);
+    }
+}
+
+
+//Iterative Method
+//TC: O(N)
+//SC: O(N) for stack
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        if(root==null)
+            return list;
+        Stack<TreeNode> s=new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode top=s.pop();
+            list.add(top.val);
+            if(top.right!=null) s.push(top.right);
+            if(top.left!=null) s.push(top.left);
+        }
+        return list;  
     }
 }
