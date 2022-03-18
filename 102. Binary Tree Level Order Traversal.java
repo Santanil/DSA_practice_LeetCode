b17e5912de3ed58725176d3212554e5568cf6ded
SANTANIL BASAK <santanilbasak@gmail.com>
2022-03-18 12:12:36 +0530

    Update 102. Binary Tree Level Order Traversal.java

--- 102. Binary Tree Level Order Traversal.java
+++ 102. Binary Tree Level Order Traversal.java
@@ -24,29 +24,38 @@
 //TC:O(N) as we are travelling all the nodes
 //SC:O(N) because of the queue as it will store all the nodes in worst case
 //but for SC we will not consider list as it is asked as o/p
-class Solution { 
+
+class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list=new ArrayList<List<Integer>>();
-        if(root==null) 
-            return list;
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null)
+            return res;
         Queue<TreeNode> q= new LinkedList<>();
-        TreeNode levelLastNode=root,front;
-        q.add(root);
+        q.offer(root);
         while(!q.isEmpty()){
-            int levelSize=q.size();
-            List<Integer> subList=new ArrayList<>();
-            for(int i=0;i<levelSize;i++){
-                front=q.peek();
-                if(front.left!=null)
-                    q.offer(front.left);
-                if(front.right!=null)
-                    q.offer(front.right);
-                subList.add(front.val);
-                q.remove(front);
+            List<Integer> list=new ArrayList<>();
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode peekElement=q.peek();
+                if(peekElement.left!=null)
+                    q.add(peekElement.left);
+                if(peekElement.right!=null)
+                    q.add(peekElement.right);
+                list.add(peekElement.val);
+                q.poll();
             }
-            list.add(subList);
+            res.add(list);
         }
-        return list;
+        return res;
     }
 }
 
+//Note:
+//Poll() - It will give the head element of the queue and will remove the head element from queue. If queue is empty then it will return null.
+
+//Remove() - It will give the head element of the queue and will remove the head element from queue. If queue is empty then it will throw an Exception.
+
+
+//offer vs add
+//The only difference between the methods is that the OFFER method throws true or false if the addition is made. As opposed to this, 
+//the ADD method throws an exception when no more additions are possible in the queue.
