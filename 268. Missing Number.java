250cfde706160ccc2ca36136319a2c7f0ba841f3
SANTANIL BASAK <santanilbasak@gmail.com>
2022-04-18 22:23:09 +0530

    Update 268. Missing Number.java

--- 268. Missing Number.java
+++ 268. Missing Number.java
@@ -1,6 +1,6 @@
 //268. Missing Number
 
-
+----------------------------Using XOR----------------------------------------
 // TC:O(N)  SC:O(1)
 class Solution {
     public int missingNumber(int[] nums) {
@@ -9,5 +9,33 @@ class Solution {
             ans=ans^nums[i]^(i+1);   
         }
         return ans;
+    }
+}
+
+
+---------------------------Using cyclic sort------------------------------------------------
+//TC:O(N)
+//SC:O(1)
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i];
+            if(nums[i]<nums.length && nums[i]!=nums[correct])
+                swap(nums,i,correct);
+            else
+                i++;
+        }
+        
+        //find the missing number by iterating through the list
+        for(i=0;i<nums.length;i++)
+            if(nums[i]!=i)
+                return i;
+        return i;
+    }
+    public void swap(int[] nums,int i,int correct){
+        int temp=nums[i];
+        nums[i]=nums[correct];
+        nums[correct]=temp;
     }
 }
