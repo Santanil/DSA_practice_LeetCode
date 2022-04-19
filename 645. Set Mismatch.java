714205175a04ae098212e7e04660b9fce1e441ca
SANTANIL BASAK <santanilbasak@gmail.com>
2022-04-19 21:51:50 +0530

    Create 645. Set Mismatch.java

---
+++ 645. Set Mismatch.java
@@ -0,0 +1,32 @@
+645. Set Mismatch
+https://www.youtube.com/watch?v=JfinxytTYFQ&t=3008s
+
+
+--------------------------------Cyclic Sort----------------------------------
+  //TC:O(N)
+//SC:O(1)
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]<=nums.length && nums[i]!=nums[correct])
+                swap(nums,i,correct);
+            else
+                i++;
+        }
+        
+        //iterating through the array and checking serially for the number that doean't match with the index value
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=i+1)
+                return new int[]{nums[i],i+1};
+        }
+        return new int[]{-1};
+    }
+    
+    public void swap(int[] arr,int i,int correct){
+        int temp=arr[i];
+        arr[i]=arr[correct];
+        arr[correct]=temp;
+    }
+}
