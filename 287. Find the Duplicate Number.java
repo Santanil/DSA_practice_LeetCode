8e98184b3b555af62380a23c349fecd1d13369fc
SANTANIL BASAK <santanilbasak@gmail.com>
2022-05-07 11:42:28 +0530

    Update 287. Find the Duplicate Number.java

--- 287. Find the Duplicate Number.java
+++ 287. Find the Duplicate Number.java
@@ -1,6 +1,31 @@
 287. Find the Duplicate Number
 
+BF:
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])
+                return nums[i];
+        }
+        return -1;
+    }
+}
 
+
+Optimized(intermediate)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i]))
+                return nums[i];
+            else
+                hs.add(nums[i]);
+        }
+        return -1;
+    }
+}
 
 ---------------------------Fast Slow Pointer Technique----------------------------------------
   
