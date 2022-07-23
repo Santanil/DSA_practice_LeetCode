38fda5b80af2116328901f2b0098322565b1e3e4
SANTANIL BASAK <santanilbasak@gmail.com>
2022-07-23 16:17:44 +0530

    Update 383. Ransom Note.java

--- 383. Ransom Note.java
+++ 383. Ransom Note.java
@@ -1,6 +1,34 @@
 https://leetcode.com/problems/ransom-note/
 
 
+***************************************************Naive*********************************************
+//TC: O(N*M)
+//SC: O(N)
+class Solution {
+    public boolean canConstruct(String ransomeNote, String magazine) {
+        StringBuilder str=new StringBuilder(magazine);
+        boolean found=false;
+        System.out.println(ransomeNote);
+        for(int i=0;i<ransomeNote.length();i++){
+            
+                    for(int j=0;j<str.length();j++){
+                        if(ransomeNote.charAt(i)==str.charAt(j)){
+                            found=true;
+                            str.deleteCharAt(j);
+                            break;
+                        }
+
+                    }
+            
+                    if(!found){
+                        return false;
+                    }
+                    found=false;
+        }
+        return true;
+    }
+}
+***************************************************Optimized*********************************************
 //TC: O(N)
 //SC: O(N)
 class Solution {
