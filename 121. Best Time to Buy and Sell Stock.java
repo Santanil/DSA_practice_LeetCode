c7ade25d7958b28bc8ee1357de67e1ce988147f5
SANTANIL BASAK <santanilbasak@gmail.com>
2022-07-24 23:24:02 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- 121. Best Time to Buy and Sell Stock.java
+++ 121. Best Time to Buy and Sell Stock.java
@@ -1,5 +1,5 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
+https://www.youtube.com/watch?v=eMSfBgbiEjk
 
 *********************************************Naive****************************************
 TC: O(N^2)
