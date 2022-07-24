10bda541b421306bcf3d9b1bbb3b17d3cca1167c
SANTANIL BASAK <santanilbasak@gmail.com>
2022-07-24 09:31:12 +0530

    Update 643. Maximum Average Subarray I.java

--- 643. Maximum Average Subarray I.java
+++ 643. Maximum Average Subarray I.java
@@ -1,6 +1,29 @@
 https://leetcode.com/problems/maximum-average-subarray-i/
 
 
+**************************************************************Naive******************************************************
+TC:O(N^2)
+SC: O(1)
+  
+  
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum=0, maxSum=Integer.MIN_VALUE; 
+        for(int i=0;i<k;i++)
+            sum+=nums[i];
+        maxSum=sum;
+        for(int i=1;i<nums.length-k+1;i++){
+            sum=0;
+            for(int j=0;j<k;j++)
+                sum+=nums[j+i];
+            maxSum=Math.max(maxSum,sum);
+        }
+        return maxSum/k;
+    }
+}
+
+************************************************Optimized*******************************************************************
+Sliding window
 TC: O(N)
 SC: O(1)
   
