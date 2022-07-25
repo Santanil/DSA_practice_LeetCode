76f3ddd691c6e3b0206e6611bfd8ff2a692ace59
SANTANIL BASAK <santanilbasak@gmail.com>
2022-07-25 23:34:28 +0530

    Create 455. Assign Cookies.java

---
+++ 455. Assign Cookies.java
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/assign-cookies/
+
+TC: O(N)
+SC: O(1)
+
+class Solution{
+    public int findContentChildren(int[] g, int[] s) {
+        int count=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int j=0;
+        for(int i=0; i<g.length; i++){
+            while(j<s.length && s[j]<g[i]) j++;
+            if(j==s.length) break;
+            count++;
+            j++;
+        }
+        return count;
+    }
+}
