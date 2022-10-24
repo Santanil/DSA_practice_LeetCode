68f790c7e4be85ed1adc61d3f24442e6a8f51441
SANTANIL BASAK <santanilbasak@gmail.com>
2022-10-24 21:56:12 +0530

    Update 20. Valid Parentheses.java

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -5,31 +5,27 @@ https://leetcode.com/problems/valid-parentheses/
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk=new Stack();
+        Stack<Character> stk=new Stack<>();
+        
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==')'){
-                if(stk.isEmpty() || stk.peek()!='(')
-                    return false;
-                stk.pop();
-            }
-            else if(s.charAt(i)=='}'){
-                    if(stk.isEmpty() || stk.peek()!='{')
-                        return false;
-                    stk.pop();
-            }
-            else if(s.charAt(i)==']'){
-                    if(stk.isEmpty() || stk.peek()!='[')
-                        return false;
-                    stk.pop();
-            }
-            else
-                stk.push(s.charAt(i));
-        }
+            char c=s.charAt(i);
             
+            if(c=='(' || c=='{' || c=='[')
+                stk.push(c);
+            else{
+                if(stk.isEmpty())
+                    return false;
+                if((c==')' && stk.peek()=='(') || (c=='}' && stk.peek()=='{') || (c==']' && stk.peek()=='['))
+                    stk.pop();
+                else
+                    return false;
+            }
+        }
         if(stk.isEmpty())
             return true;
-        else
-            return false;
-            
+        return false;
     }
 }
+
+
+
