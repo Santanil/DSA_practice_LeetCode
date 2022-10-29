8123c4c2f0fe002d29f6bc66a3ba00552ef5e5ff
SANTANIL BASAK <santanilbasak@gmail.com>
2022-10-29 19:58:52 +0530

    Update 42. Trapping Rain Water.java

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -1,35 +1,57 @@
 https://leetcode.com/problems/trapping-rain-water/
+https://www.youtube.com/watch?v=m18Hntz4go8
 
-//TC: O(N)
-//SC: O(N)
+TC: O(N)
+SC: O(N)
 
 class Solution {
     public int trap(int[] height) {
-        int leftMax=0,rightMax=0,n=height.length,resultAmt=0;
-        if(n<2)
-            return 0;
-        int[] leftArr=new int[n];
-        int[] rightArr=new int[n];
+        int l=height.length,sum=0;
+        int[] leftMax=new int[l];
+        int[] rightMax=new int[l];
         
+        leftMax[0]=height[0];
+        rightMax[l-1]=height[l-1];
+        for(int i=1;i<l;i++)
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
         
-        //leftArr stores the max height present to its left
-        for(int i=1;i<n;i++){
-            leftMax=Math.max(height[i-1],leftMax);
-            leftArr[i]=leftMax;
+        for(int i=l-2;i>=0;i--)
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        
+        for(int i=0;i<l;i++){
+            int water=Math.min(leftMax[i],rightMax[i])-height[i];
+            if(water>0)
+                sum+=water;
         }
-        
-        //rightArr stores the max height present to its right
-        for(int i=n-2;i>=0;i--){
-            rightMax=Math.max(height[i+1],rightMax);
-            rightArr[i]=rightMax;
+        return sum;
+    }
+}
+
+
+---------------------------------------------------------Optimized----------------------------------------------------------
+   TC: O(N)
+   SC: O(1)
+    
+    class Solution {
+    public int trap(int[] height) {
+        int len=height.length;
+        int i=0,l=0,r=len-1,leftMax=0,rightMax=0,water=0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(height[l]>=leftMax)
+                    leftMax=height[l];
+                else
+                    water+=leftMax-height[l];
+                l++;
+            }else{
+                if(height[r]>=rightMax)
+                    rightMax=height[r];
+                else
+                    water+=rightMax-height[r];
+                r--;
+            }
+            i++;
         }
-        
-        //computes the result by finding out the diff between min among the max height on both sides and its current height
-        for(int i=1;i<n-1;i++){
-            int capacity=Math.min(leftArr[i],rightArr[i]);
-            if(capacity>height[i])
-                resultAmt+=capacity-height[i];
-        }
-        return resultAmt;
+        return water;
     }
 }
