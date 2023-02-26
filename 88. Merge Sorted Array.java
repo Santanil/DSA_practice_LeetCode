b578ba701c966a74ce4033a0179faea7acb1f3af
SANTANIL BASAK <santanilbasak@gmail.com>
2023-02-26 18:52:12 +0530

    Update 88. Merge Sorted Array.java

--- 88. Merge Sorted Array.java
+++ 88. Merge Sorted Array.java
@@ -2,52 +2,39 @@
 //https://leetcode.com/problems/merge-sorted-array/
 //https://www.youtube.com/watch?v=hVl2b3bLzBw&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=5
 
-//BF  TC:O(m+n)   SC: O(m+n)
-// class Solution {
-//     public void merge(int[] nums1, int m, int[] nums2, int n) {
-//         int arr[]=new int[m+n];
-//         int i=0,j=0,k=0;
-//         if(n!=0){
-//             while(i<m || j<n){
-//                 if(j>=n){
-//                     arr[k]=nums1[i];
-//                     i++;
-//                     k++;
-//                 }else if(i>=m){
-//                     arr[k]=nums2[j];
-//                     j++;
-//                     k++;
-//                 }else if(nums1[i]>nums2[j]){
-//                     arr[k]=nums2[j];
-//                     j++;
-//                     k++;
-//                 }else{
-//                     arr[k]=nums1[i];
-//                     i++;
-//                     k++;
-//                 }
-//             }
-//             for(i=0;i<m+n;i++)
-//                 nums1[i]=arr[i];
-//         }
-//     }
-// }
-
-
-//  TC: O( m*nlogn + n) SC: O(1)
+*****************************************BF  TC:O(m+n)   SC: O(m+n)****************************************
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0,j=0,k=0;
+        int i=0,j=0,c=0;
+        int[] res=new int[m+n];
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j])
+                res[c++]=nums1[i++];
+            else
+                res[c++]=nums2[j++];
+        }
+        while(i<m)
+            res[c++]=nums1[i++];
+        while(j<n)
+            res[c++]=nums2[j++];  
+        for(i=0;i<m+n;i++)
+            nums1[i]=res[i];
+    }
+}
+
+*****************************************Space Optimized  (No extra space)****************************************
+  TC: O( m*nlogn + n) SC: O(1)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0,j=0;
         if(n!=0){
             while(i<m || j<n){
-                if(i>=m){
+                if(i>=m){   //when all elements of nums1 are traversed and already have nums2 sorted
                     nums1[i]=nums2[j];
                     i++;
                     j++;
-                    k++;
                 }else if(nums1[i]<=nums2[j]){
                     i++;
-                    k++;
                 }else{
                     int temp=nums1[i];
                     nums1[i]=nums2[j];
