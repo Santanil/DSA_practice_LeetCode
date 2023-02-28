dd1e2c41092f2ba66d6fff1928459783d6f720b5
SANTANIL BASAK <santanilbasak@gmail.com>
2023-02-28 21:30:40 +0530

    Update 160. Intersection of Two Linked Lists.java

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -15,28 +15,51 @@
  *     }
  * }
  */
-//Not Optimized
+*************************************************Not Optimized*********************************************
 //TC:O(N)   SC:O(N)
-// public class Solution {
-//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-//         HashSet<ListNode> hs=new HashSet<>();
-//         ListNode cur=headA;
-//         while(cur!=null){
-//             hs.add(cur);
-//             cur=cur.next;
-//         }
-//         cur=headB;
-//         while(cur!=null){
-//             if(hs.contains(cur))
-//                 return cur;
-//             cur=cur.next;
-//         }
-//         return null;
-//     }
-// }
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> hs=new HashSet<>();
+        ListNode cur=headA;
+        while(cur!=null){
+            hs.add(cur);
+            cur=cur.next;
+        }
+        cur=headB;
+        while(cur!=null){
+            if(hs.contains(cur))
+                return cur;
+            cur=cur.next;
+        }
+        return null;
+    }
+}
 
 
-//Optimized
+****************************************Not Optimized Single PASS*******************************************
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> hs=new HashSet<>();
+        ListNode hA=headA,hB=headB;
+        while(hA!=null || hB!=null){
+            if(hA!=null && hs.contains(hA))
+                return hA;
+            else if(hA!=null)
+                hs.add(hA);
+            if(hB!=null && hs.contains(hB))
+                return hB;
+            else if(hB!=null)
+                hs.add(hB);
+            if(hA!=null)
+                hA=hA.next;
+            if(hB!=null)
+                hB=hB.next; 
+        }
+        return null;     
+    }
+}
+
+*********************************************Optimized*******************************************************
 //TC: O(N)   SC:O(1)
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
